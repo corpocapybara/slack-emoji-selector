1f23ddfa9262d1968fe63669488ad87340f3cebb
corpocapybara <kkcysio@gmail.com>
2017-05-25 14:56:29 +0200

    Get emojis from first found tab or opens last used emoji slack page in new tab and fetches them

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const slackEmojisRegex = /^http[s]?:\/\/[^:\/\s.]+\.slack\.com\/customize\/emoji$/;
+const slackEmojiUrlPattern = 'https://*.slack.com/customize/emoji';
 
 chrome.browserAction.onClicked.addListener(run);
 
@@ -10,15 +11,35 @@ function setClipboardContent(content) {
     copySrc.focus();
     document.execCommand('SelectAll');
     document.execCommand('Copy');
-    document.body.removeChild(copyFrom);
+    document.body.removeChild(copySrc);
 }
 
 function getEmojis(tab) {
     chrome.tabs.executeScript(tab.id, { file: 'page-script.js' }, (results) => setClipboardContent(results[0]));
 }
 
-function run(tab) {
-    if (slackEmojisRegex.test(tab.url)) {
+function tabUpdateListener(tabId, changeInfo, tab) {
+    if (slackEmojisRegex.test(tab.url) && changeInfo.status === 'complete') {
+        getEmojis(tab);
+        chrome.tabs.onUpdated.removeListener(tabUpdateListener);
+    }
+}
+
+function openSlackFromHistory(historyItems) {
+    let lastUsedSlackEmojiPage = historyItems[0].url;
+    chrome.tabs.onUpdated.addListener(tabUpdateListener);
+    chrome.tabs.create({ url: lastUsedSlackEmojiPage });
+}
+
+function getEmojisPage(tabs) {
+    let tab = tabs[0];
+    if (!tab) { 
+        chrome.history.search({ text: slackEmojiUrlPattern, maxResults: 1 }, openSlackFromHistory);
+    } else {
         getEmojis(tab);
     }
+}
+
+function run() {
+    chrome.tabs.query({ url: 'https://*.slack.com/customize/emoji' }, getEmojisPage);
 }
